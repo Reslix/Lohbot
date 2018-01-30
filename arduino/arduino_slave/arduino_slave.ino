d402996b4c715f8261854a15d0cfa472165a3bba
Kyle Montemayor <kmonte@umd.edu>
2018-01-30 15:41:33 -0500

    first version of motor controller code

--- arduino/arduino_slave/arduino_slave.ino
+++ arduino/arduino_slave/arduino_slave.ino
@@ -1,9 +1,43 @@
+/*
+ * Arduino code for 408i
+ * 
+ */
+
+
+//Pins
+const int left_forward = 10;
+const int left_backward = 11;
+const int left_pwm = 5;
+
+const int right_forward = 8;
+const int right_backward = 9;
+const int right_pwm = 3;
+
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(left_forward, OUTPUT);
+  pinMode(left_backward, OUTPUT);
+  pinMode(right_forward, OUTPUT);
+  pinMode(right_backward, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(left_forward, LOW);
+  digitalWrite(left_backward, HIGH);
+
+  digitalWrite(right_forward, LOW);
+  digitalWrite(right_backward, HIGH);
+
+
+  for(int i = 0; i < 255; i++){
+    analogWrite(left_pwm, i);
+    analogWrite(right_pwm, i);
+
+    delay(250);
+  }
+  
 
 }
