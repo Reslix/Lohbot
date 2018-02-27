f0833b7b42884f3e562653e33128ffab09127911
Kyle Montemayor <kmonte@umd.edu>
2018-02-27 14:37:13 -0500

    communication header file

---
+++ arduino/arduino_slave/lohmunication.hpp
@@ -0,0 +1,44 @@
+/*
+ * Serial upstream/downstream communication
+ * Kyle Montemayor
+ */
+#ifndef __LOHMUNICATION_H__
+#define __LOHMUNICATION_H__
+
+#include "movement.hpp"
+/*
+ * Want to have enums for the different message types, and then for data inside 
+ * the messages. Because we use ints for data can jenk that instead of the enum
+ * if needed
+ */
+
+enum commands {move, get_dist};
+
+union Data {
+    int i;
+    float f;
+    Movement::turn_dir;    
+} data;
+
+typedef struct {
+	commands command;
+	data data;
+} message;
+
+
+/* Also want a queue for the commands, too lazy to implement separately */
+// Kind of arbitrary but not the biggest deal
+
+#define MESSAGE_QUEUE_LENGTH 128
+static int queue_pos = 0;
+static int new_commands = 0;
+
+message message_queue[MESSAGE_QUEUE_LENGTH];
+
+int fill_buffer();
+
+void run_commands();
+
+void run_command(message);
+#endif
+
