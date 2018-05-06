8db2bff1c422bb4e59b735646877797707abe0af
Andrew T. Wang <wangandrewt@gmail.com>
2018-05-06 18:54:06 -0400

    Improved streaming server
    Send state and tracking image instead of plain camera image

--- jetson/flask_streaming_server.py
+++ jetson/flask_streaming_server.py
@@ -1,9 +1,19 @@
 import multiprocessing
+from multiprocessing.managers import SyncManager
 import time
 
+import fasteners
 from flask import Blueprint, Flask, render_template, Response, send_file
 import gunicorn.app.base
 from gunicorn.six import iteritems
+
+
+class ImageManager(SyncManager):
+    """
+    Controls access to shared dict object
+    """
+    pass
+
 
 """
 Starts a Flask server to listen to streaming requests
@@ -11,27 +21,48 @@ Starts a Flask server to listen to streaming requests
 
 app = Flask(__name__)
 
+lock_file_name = 'ALEXA_COMMAND.txt.lock'
+file_name = 'ALEXA_COMMAND.txt'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def gen(camera):
-    if camera == None:
+def gen(manager):
+    if manager == None:
         return
     while True:
-        frame = camera.get_jpg()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        image_dictionary = manager.get_dict()
+        if 'encoded' in image_dictionary.keys():
+            frame = image_dictionary.get('encoded')
+            yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        else:
+            yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + b'\0' + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(app.config['CAMERA']),
+    return Response(gen(app.config['MANAGER']),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/status')
 def send_status():
-    return send_file("ALEXA_COMMAND.txt")
+    output = "Alexa command: "
+    with fasteners.InterProcessLock(lock_file_name):
+        with open(file_name, "r") as file:
+            output = output + file.read().replace('\n', '')
+    manager = app.config['MANAGER']
+    if manager == None:
+        return output
+
+    image_dictionary = manager.get_dict()
+    if 'state' in image_dictionary.keys():
+        state = image_dictionary.get('state')
+        output = output + "<br>Tracker state: " + state
+
+    return output
+
 
 class StandaloneApplication(gunicorn.app.base.BaseApplication):
     """Class for starting custom Gunicorn WSGI application"""
@@ -52,9 +83,10 @@ class StandaloneApplication(gunicorn.app.base.BaseApplication):
 
 def start_streaming_server(c):
     """
-    c -- Camera object (from camera.py)
+    :param c: SyncManager with member get_dict that is a shared Proxy Object
+    See https://docs.python.org/3.6/library/multiprocessing.html#multiprocessing.managers.SyncManager
     """
-    app.config['CAMERA'] = c
+    app.config['MANAGER'] = c
 
     options = {
         'bind': '%s:%s' % ('localhost', '11578'),
--- jetson/runner_no_arduino.py
+++ jetson/runner_no_arduino.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import math, time
-from multiprocessing import Process
+from multiprocessing import Manager, Process
 
 from serial_io import SerialIO
 from camera import TrackingCameraRunner
 from flask_streaming_server import start_streaming_server
+from flask_streaming_server import ImageManager
 from show import imshow
 import cv2
 import fasteners
@@ -12,11 +13,18 @@ import fasteners
 
 if __name__ == "__main__":
 
+    # Initialize camera
     c = TrackingCameraRunner(0)
     camera = c.camera
     c.step_frame()
+
+    # Shared Manager object
+    image_dictionary = Manager().dict()
+    ImageManager.register('get_dict', callable=lambda:image_dictionary)
+    manager = ImageManager()
+
     # Start web server to stream camera image
-    p = Process(target=start_streaming_server, args=(camera,))
+    p = Process(target=start_streaming_server, args=(manager,))
     p.start()
 
     im = None
@@ -37,14 +45,26 @@ if __name__ == "__main__":
                 size = math.sqrt(rect[2]**2+rect[3]**2)
 
                 print('Moving')
+                manager.get_dict().update([('state', 'follow: moving')])
             else:
                 print('Stopping')
                 im = imshow(image, im=im)
+                manager.get_dict().update([('state', 'follow: stopping')])
+
+            encoded = cv2.imencode('.jpg', image)[1].tostring()
+            manager.get_dict().update([('encoded', encoded)])
+
         elif command == 'stop':
             print('stop')
+            encoded = camera.get_jpg()
+            manager.get_dict().update([('encoded', encoded)])
+            manager.get_dict().update([('state', 'stopping')])
         elif command == 'openpose':
             print('Openpose')
+            manager.get_dict().update([('state', 'Openpose')])
             pass
         else:
             print('undefined command')
+            manager.get_dict().update([('state', 'undefined command')])
+
         time.sleep(1)
