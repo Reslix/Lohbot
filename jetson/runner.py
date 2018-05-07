f9c5b6c39b8ad8579927df4f374bc160186860b9
Andrew T. Wang <wangandrewt@gmail.com>
2018-05-07 12:43:51 -0400

    Update runner

--- jetson/runner.py
+++ jetson/runner.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import math, time
-from multiprocessing import Process
+from multiprocessing import Manager, Process
 
 from serial_io import SerialIO
 from camera import TrackingCameraRunner
 from flask_streaming_server import start_streaming_server
+from flask_streaming_server import ImageManager
 from show import imshow
 import cv2
 import fasteners
@@ -16,12 +17,21 @@ if __name__ == "__main__":
     ard = SerialIO()
     ard.start()
 
+    # Initialize camera
     c = TrackingCameraRunner(0)
     camera = c.camera
 
     c.step_frame()
+
+    # Shared Manager object
+    image_dictionary = Manager().dict()
+    ImageManager.register('get_dict', callable=lambda:image_dictionary)
+    manager = ImageManager()
+    manager.start()
+    manager.get_dict().update([('camera', camera)])
+
     # Start web server to stream camera image
-    p = Process(target=start_streaming_server, args=(camera,))
+    p = Process(target=start_streaming_server, args=(manager,))
     p.start()
 
     im = None
@@ -42,13 +52,25 @@ if __name__ == "__main__":
                 left = distance + differential
                 right = distance - differential
                 ard.direct(int(left), int(right))
+
+                manager.get_dict().update([('state', 'follow: moving')])
             else:
                 ard.stop()
+                manager.get_dict().update([('state', 'follow: stopping')])
+
+            # Update mangager with shared image
+            # encoded = cv2.imencode('.jpg', image)[1].tostring()
+            # manager.get_dict().update([('encoded', encoded)])
         elif command == 'stop':
             ard.stop()
+            print('stop')
+            # encoded = camera.get_jpg()
+            # manager.get_dict().update([('encoded', encoded)])
+            manager.get_dict().update([('state', 'stopping')])
         elif command == 'openpose':
             #TODO
-            pass
+            print('Openpose')
+            manager.get_dict().update([('state', 'Openpose')])
         else:
             print('undefined command')
-
+            manager.get_dict().update([('state', 'undefined command')])
