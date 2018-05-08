ee84a7302ba219691269f68ce395f0511a29db8b
Andrew T. Wang <wangandrewt@gmail.com>
2018-05-08 15:29:38 -0400

    Ball tracking

--- jetson/ballrunner.py
+++ jetson/ballrunner.py
@@ -18,8 +18,10 @@ while True:
         differential = (tcenterx - center[0])//3
         distance = tradius - radius
         left = distance + differential
+        left = max(-30, min(30, left))
         right = distance - differential
+        right = max(-30, min(30, right))
         print(left,right)
-        ard.direct(int(left), int(right))
+        ard.direct(int(right), int(left))
     else:
         ard.stop()
